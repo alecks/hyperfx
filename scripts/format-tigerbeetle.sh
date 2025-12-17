939081533737eab63815f768d7c757f32e2cabe5
Alex Pedley <alex@pedley.org>
2025-12-17 22:13:37 +0000

    add development scripts

---
+++ scripts/format-tigerbeetle.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./tigerbeetle format --cluster=0 --replica=0 --replica-count=1 --development ./data/0_0.tigerbeetle
---
+++ scripts/start-tigerbeetle.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./tigerbeetle start --addresses=3000 --development ./data/0_0.tigerbeetle
+
