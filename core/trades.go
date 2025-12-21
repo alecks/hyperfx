bde5501606dc31a0c86d113673049b80c6cb3d50
Alex Pedley <alex@pedley.org>
2025-12-21 13:07:29 +0000

    use shopspring decimals for quotes

--- core/trades.go
+++ core/trades.go
@@ -34,52 +34,40 @@ func (c *Core) GetRate(ledger Ledger) (decimal.Decimal, error) {
 
 func (c *Core) LocalFromForeign(
 	direction TradeDirection,
-	foreignAmount uint64,
+	foreignAmount decimal.Decimal,
 	foreignLedger Ledger,
-) (uint64, error) {
-	foreignScale := CurrencyAssetScales[foreignLedger]
+) (decimal.Decimal, error) {
 	localScale := CurrencyAssetScales[c.options.LocalCurrencyLedger]
 
 	rate, err := c.GetRate(foreignLedger)
 	if err != nil {
-		return 0, err
+		return decimal.Zero, err
 	}
 
-	// Shift by the negative asset scale to get our 'standard form' decimal
-	foreignDec := decimal.NewFromUint64(foreignAmount).Shift(-foreignScale)
-
 	if direction == TradeSell {
-		localDec := foreignDec.DivRound(rate, HfxPrecision)
-		// Shift back to minor units and Ceil to nearest minor unit in our favour (see https://docs.tigerbeetle.com/single-page/#coding-recipes-currency-exchange)
-		return uint64(localDec.Shift(localScale).Ceil().IntPart()), nil
-
+		// Ceil to nearest minor unit in our favour (see https://docs.tigerbeetle.com/single-page/#coding-recipes-currency-exchange)
+		return foreignAmount.DivRound(rate, HfxPrecision).RoundCeil(localScale), nil
 	} else {
-		localDec := foreignDec.Mul(rate)
-		// Shift back to minor units and Floor in our favour
-		return uint64(localDec.Shift(localScale).Floor().IntPart()), nil
+		// Floor in our favour
+		return foreignAmount.Mul(rate).RoundFloor(localScale), nil
 	}
 }
 
 func (c *Core) ForeignFromLocal(
 	direction TradeDirection,
-	localAmount uint64,
+	localAmount decimal.Decimal,
 	foreignLedger Ledger,
-) (uint64, error) {
-	localScale := CurrencyAssetScales[c.options.LocalCurrencyLedger]
+) (decimal.Decimal, error) {
 	foreignScale := CurrencyAssetScales[foreignLedger]
 
 	rate, err := c.GetRate(foreignLedger)
 	if err != nil {
-		return 0, err
+		return decimal.Zero, err
 	}
 
-	localDec := decimal.NewFromUint64(localAmount).Shift(-localScale)
-
 	if direction == TradeSell {
-		foreignDec := localDec.Mul(rate)
-		return uint64(foreignDec.Shift(foreignScale).Floor().IntPart()), nil
+		return localAmount.Mul(rate).RoundFloor(foreignScale), nil
 	} else {
-		foreignDec := localDec.DivRound(rate, HfxPrecision)
-		return uint64(foreignDec.Shift(foreignScale).Ceil().IntPart()), nil
+		return localAmount.DivRound(rate, HfxPrecision).RoundCeil(foreignScale), nil
 	}
 }
